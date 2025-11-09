3a87b88de53541ba488d73ca92ef45cc45616e3a
Rokusuke69 <chauhan.abhishek.motilal@gmail.com>
2025-11-09 20:38:48 +0530

    first commit

---
+++ my-first-app/index.js
@@ -0,0 +1,7 @@
+// You'll need to import it first (using CommonJS syntax for now)
+const chalk = require('chalk');
+
+console.log(chalk.green('Namaste Duniya!'));
+console.log(chalk.blue.bold('This is now in blue and bold!'));
+
+// console.log("Namaste Duniya! (Hello World) ... and Goodbye World!");
